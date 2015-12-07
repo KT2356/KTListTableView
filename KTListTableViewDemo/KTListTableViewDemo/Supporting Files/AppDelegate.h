8a4180fdf51cb898ec66c59879baa890cd727276
KT2356 <15152112356@163.com>
2015-12-07 17:32:46 +0800

    add file

---
+++ KTListTableViewDemo/KTListTableViewDemo/Supporting Files/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KTListTableViewDemo
+//
+//  Created by KT on 15/12/4.
+//  Copyright © 2015年 KT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
